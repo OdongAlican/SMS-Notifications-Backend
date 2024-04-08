e4e49ce58b59acd3562a7e6efd1016b15271fcbb
Bogere <bogeresiraje25@gmail.com>
2024-04-08 07:03:15 +0300

    Unauthenticated email service

--- pride_notify_service/env/local.example.py
+++ pride_notify_service/env/local.example.py
@@ -2,3 +2,12 @@
 SMS_URL = ""
 SMS_API_ID = ""
 SMS_SENDER_ID = ""
+
+# email notice settings
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = ''
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
+EMAIL_USE_TLS = True
