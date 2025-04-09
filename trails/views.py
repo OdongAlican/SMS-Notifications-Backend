fbf53ceb353469de5c7ae4a1c74e1cb215a3f8dd
Odong Sunday <sandieo.2020@gmail.com>
2025-04-09 09:25:15 +0300

    fix layout

--- trails/views.py
+++ trails/views.py
@@ -1,8 +1,14 @@
-# views.py
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 from .models import AuditTrail
 from .serializers import AuditTrailSerializer
+
+class CustomPageNumberPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 class AuditTrailViewSet(viewsets.ModelViewSet):
     queryset = AuditTrail.objects.all().select_related('user').filter(user__isnull=False)
     serializer_class = AuditTrailSerializer
+    pagination_class = CustomPageNumberPagination
