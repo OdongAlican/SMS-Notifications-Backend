101b1faa96f5da5d67b72715332eceaea9e030a9
Odong Sunday <sandieo.2020@gmail.com>
2025-04-09 09:55:54 +0300

    fix trails

--- trails/views.py
+++ trails/views.py
@@ -2,7 +2,8 @@ from rest_framework import viewsets
 from rest_framework.pagination import PageNumberPagination
 from .models import AuditTrail
 from .serializers import AuditTrailSerializer
-
+from rest_framework.permissions import IsAuthenticated
+from users.utils import CustomGroupPermission
 class CustomPageNumberPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
@@ -11,4 +12,5 @@ class CustomPageNumberPagination(PageNumberPagination):
 class AuditTrailViewSet(viewsets.ModelViewSet):
     queryset = AuditTrail.objects.all().select_related('user').filter(user__isnull=False)
     serializer_class = AuditTrailSerializer
+    permission_classes = [IsAuthenticated, CustomGroupPermission]
     pagination_class = CustomPageNumberPagination
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 import secrets
 from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import Group, Permission
-from rest_framework import viewsets, generics
+from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
