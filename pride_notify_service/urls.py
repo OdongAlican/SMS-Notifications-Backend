8a6eb0eb4a0d2d2f067081288eef33241ce02ffd
Odong Sunday <sandieo.2020@gmail.com>
2025-04-28 18:18:25 +0300

    fix api

--- pride_notify_service/urls.py
+++ pride_notify_service/urls.py
@@ -14,19 +14,19 @@ from users.authentication import (
 from users.forgotPassword import ForgotPasswordRequestApi, ResetPasswordApi
 
 urlpatterns = [
-    path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
-    path('change-password/', PasswordResetRequestView.as_view(), name='change-password'),
-    path('reset-temporary-password/', ResetTemporaryPasswordApi.as_view(), name='reset_temporary_password'),
-    path('unlock-user-account/', UnlockUserAccountView.as_view(), name='unlock-account'),
-    path('forgot-password/', ForgotPasswordRequestApi.as_view(), name='forgot-password'),
-    path('reset-password/', ResetPasswordApi.as_view(), name='reset-password'),
-    path('deactivate-user/', DeactivateUserAccountView.as_view(), name='deactivate-user'),
-    path('activate-user/', ActivateUserAccountView.as_view(), name='activate-user'),
-    path('admin/', admin.site.urls),
-    path('sms/', Sms.as_view()),
-    path('email/', Email.as_view()),
-    path('data/', include('users.urls')),
-    path('trails/', include('trails.urls')),
-    path('logs/', include('pride_notify_notice.urls'))
+    path('api/v1/login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', CustomTokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/change-password/', PasswordResetRequestView.as_view(), name='change-password'),
+    path('api/v1/reset-temporary-password/', ResetTemporaryPasswordApi.as_view(), name='reset_temporary_password'),
+    path('api/v1/unlock-user-account/', UnlockUserAccountView.as_view(), name='unlock-account'),
+    path('api/v1/forgot-password/', ForgotPasswordRequestApi.as_view(), name='forgot-password'),
+    path('api/v1/reset-password/', ResetPasswordApi.as_view(), name='reset-password'),
+    path('api/v1/deactivate-user/', DeactivateUserAccountView.as_view(), name='deactivate-user'),
+    path('api/v1/activate-user/', ActivateUserAccountView.as_view(), name='activate-user'),
+    path('api/v1/admin/', admin.site.urls),
+    path('api/v1/sms/', Sms.as_view()),
+    path('api/v1/email/', Email.as_view()),
+    path('api/v1/data/', include('users.urls')),
+    path('api/v1/trails/', include('trails.urls')),
+    path('api/v1/logs/', include('pride_notify_notice.urls'))
 ]
